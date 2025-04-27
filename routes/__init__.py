d3079c053b5f8d85023b90241e46448fdba21926
emreayann <emreayann321@gmail.com>
2025-04-27 23:24:37 +0300

    mongodb connectıon added and relational things added

---
+++ routes/__init__.py
@@ -0,0 +1,7 @@
+from .auth import auth_bp
+from .profile import profile_bp
+from .products import products_bp
+from .cart import cart_bp
+from .dev import dev_bp
+
+__all__ = ['auth_bp', 'profile_bp', 'products_bp', 'cart_bp', 'dev_bp'] 
